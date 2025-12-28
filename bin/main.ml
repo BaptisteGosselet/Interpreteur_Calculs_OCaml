9cc72a9cd14cae81317eb9f9b50ef14f6154b24f
Baptiste Gosselet <baptiste@b-gosselet.localdomain>
2025-12-28 16:10:48 +0100

    handle spaces in tokenization

--- bin/main.ml
+++ bin/main.ml
@@ -3,16 +3,10 @@ open Calculatrice.Lexer
 open Calculatrice.Eval
 open Calculatrice.Parser 
 
-(*
 let readUserInput () : string =
   print_endline "Entrer une expression : ";
   read_line ()
-*) 
+
 
 let () =
-  let s = "-4*(-3+2)" in (*todo: spaces*)
-  print_float (eval (parseTokens (tokenize s)))
-
-  (* Ici, il faudra ajouter constructExpr pour créer l'arbre *)
-  (* let ast = constructExpr tokens in *)
-  (* print_float (eval ast) *)
+  print_float (eval (parseTokens (tokenize (readUserInput ()))))
--- lib/lexer.ml
+++ lib/lexer.ml
@@ -2,10 +2,10 @@ open Types
 
 let morphemsRegexDelimiters = "\\([0-9]+\\(\\.[0-9]+\\)?\\|[()+\\-*/]\\)"
 
-let rec afficheStrList (lst:(string list)): unit =
+let rec afficheStrList (lst : string list) : unit =
   match lst with
   | [] -> ()
-  | e::rst -> print_endline ("elem:" ^ e); afficheStrList rst
+  | e :: rst -> print_endline ("elem:" ^ e); afficheStrList rst
 
 let afficheTokenList (tokens : token list) : unit =
   tokens
@@ -20,30 +20,36 @@ let afficheTokenList (tokens : token list) : unit =
   |> String.concat " "
   |> print_endline
 
-let splitMorphems (s:string) : string list =
+let splitMorphems (s : string) : string list =
   let matches = Str.full_split (Str.regexp morphemsRegexDelimiters) s in
   List.rev (
     List.fold_left (fun acc e ->
       match e with
-      | Str.Text t when t <> "" -> t :: acc
+      | Str.Text t -> 
+          let trimmed = String.trim t in
+          if trimmed = "" then acc else trimmed :: acc
       | Str.Delim d -> d :: acc
-      | _ -> acc
     ) [] matches
   )
 
-let tokenize (s:string) : token list =
+let tokenize (s : string) : token list =
   let lst = splitMorphems s in
   List.rev (
     List.fold_left (fun acc e ->
-      match float_of_string_opt e with
-      | Some nb -> TNombre nb :: acc
-      | None -> match e with
-        | "(" -> TParG :: acc
-        | ")" -> TParD :: acc
-        | "+" -> TPlus :: acc
-        | "-" -> TMoins :: acc
-        | "*" -> TFois :: acc
-        | "/" -> TDiv :: acc
-        | _ -> failwith ("Token non reconnu : " ^ e)
+      (* Ignore les espaces *)
+      let trimmed = String.trim e in
+      if trimmed = "" then acc
+      else
+        match float_of_string_opt trimmed with
+        | Some nb -> TNombre nb :: acc
+        | None -> 
+            (match trimmed with
+             | "(" -> TParG :: acc
+             | ")" -> TParD :: acc
+             | "+" -> TPlus :: acc
+             | "-" -> TMoins :: acc
+             | "*" -> TFois :: acc
+             | "/" -> TDiv :: acc
+             | _ -> failwith ("Token non reconnu : '" ^ trimmed ^ "'"))
     ) [] lst
   )
